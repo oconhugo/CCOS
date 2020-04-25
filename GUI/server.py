a599daef2d4eda7f1d92545f168178ced2f81c2e
oconhugo <hugo.ocon@live.com>
2020-04-25 16:24:38 -0600

    Add files via upload

--- GUI/server.py
+++ GUI/server.py
@@ -1,22 +1,3 @@
-import socket
-import pickle
 
-
-class server(object):
-    def __init__(self, master, **kwargs):
-        print("3")
-
-    def server_socket(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        port = 3125
-        s.bind(('0.0.0.0', port))
-        print ('Socket binded to port 3125')
-        s.listen(3)
-        print ('socket is listening')
-
-        while True:
-            c, addr = s.accept()
-            print('Got connection from ', addr)
-            print(pickle.loads(c.recv(1024)))
 
 
